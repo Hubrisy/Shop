eac5d206b3c9c0c77d95a19c4ec9c8e85ef5f83c
Your Name <Hubrisy>
2023-01-02 14:54:03 +0200

    modal window

--- src/components/links/LinksContacts.jsx
+++ src/components/links/LinksContacts.jsx
@@ -1,12 +1,19 @@
 import telephone from '../../images/telephone.svg'
 import bag from '../../images/bag.svg'
+
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
+
+import ModalWindow from '../modalWindow/ModalWindow';
 
 function LinksContacts() {
+
+    const [modalActive,setModalActive] = useState(false);
+
     return (
         <div className='links__contacts-bag-container'>
             <div className='links__contacts-and-bag'>
-                <div className='links__phone'>
+                <div className='links__phone' onClick={() => setModalActive(true)}>
                     <img src={telephone} alt='' />
                     <span>+380-12-3456-789</span>
                 </div>
@@ -14,6 +21,7 @@ function LinksContacts() {
                     <Link to='/bag'><img src={bag} alt='' /></Link>
                 </div>
             </div>
+            <ModalWindow active={modalActive} setActive={setModalActive}/>
         </div>
     )
 }
